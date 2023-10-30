ddb1e51449c6f47ed99f780cf68ef2a2777132b1
P2101 <pferre11@gmx.com>
2023-10-30 17:17:35 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function iniciarMap(){
     var coord = {lat:40.0052431 ,lng: 3.8491911};
     var map = new google.maps.Map(document.getElementById('map'),{
@@ -9,16 +8,4 @@ function iniciarMap(){
       position: coord,
       map: map
     });
-=======
-function iniciarMap(){
-    var coord = {lat:40.0052431 ,lng: 3.8491911};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 14,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
->>>>>>> 35e0eb240126260f8c4b6a161d4c96519241fb66
 }
